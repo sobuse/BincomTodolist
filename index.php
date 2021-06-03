746665eb0ba20be0e33d28a90f61c1084987c281
sammy <ehuerikenbaba@gmail.com>
2021-06-03 21:06:49 +0100

    added styling

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
@@ -21,8 +25,42 @@
       
         <table class="table">
          
-         <button type="button" class="btn btn-success">Add Task</button><button type="button" class="btn btn-defualt pull-right" >Print</button>
+         <button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-success">Add Task</button>
+         <button type="button" class="btn btn-defualt pull-right" >Print</button>
          <hr/><br/>
+         
+         <!-- Trigger the modal with a button -->
+<!-- <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button> -->
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Add Task</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+        <div class="form-group">
+        <label>Task Name</label>
+        <input type="text" Required name="task" class="form-control"/>
+        </div>
+        <input type="submit" name="send" value="send" class="btn btn-success" />
+        
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
   <thead>
 
     <tr>
@@ -34,8 +72,10 @@
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
+      <td class="col-md-10">Mark</td>
+      <td><a href="" class="btn btn-success">Edit</a></td>
+      <td><a href="" class="btn btn-danger">Delete</a></td>
+      
       
     </tr>
     
